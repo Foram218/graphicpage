2b6b6f578ba1388ca9724435b56e0bf1dd39fb5c
iMac <imac@iMac-2.local>
2023-10-05 14:24:12 +0530

    graphic

--- src/components/screens/Brochure.js
+++ src/components/screens/Brochure.js
@@ -3,7 +3,7 @@ import brochure2 from "../images/brochure2.svg";
 import styles from "./Easi.module.css";
 import Header from "../common/Header/Header";
 import "../images/brochure.png";
-import ClientsReview from "../images/ClientsReview.svg";
+// import ClientsReview from "../images/ClientsReview.svg";
 
 const Brochure = () => {
   return (
@@ -13,7 +13,7 @@ const Brochure = () => {
         <img src={brochure2} alt="" className={styles.image} />
         <div className="text-left px-12 ">
           <p className={styles.title}>Brochure</p>
-          <p className={`${styles.text} my-5`}>Project URL :</p>
+          {/* <p className={`${styles.text} my-5`}>Project URL :</p> */}
           <p className={`${styles.text} my-5`}>Categories : Brochure</p>
           <p className={styles.text}>Tools : Figma</p>
         </div>
